446c858adb3957b519ed0282405520ba67b2d689
elMestre <carmesce@gmail.com>
2025-06-12 08:20:42 +0200

    Lectura desde fichero

--- bola_magica.py
+++ bola_magica.py
@@ -1,9 +1,20 @@
 import random
 
+FICHERO = "frases.txt"
+
+
+def respuestas_desde(FICHERO: str):
+    respuestas = []
+    with open(FICHERO, 'r', encoding='utf-8') as fichero:
+        for linea in fichero:
+            respuestas.append(linea.strip())
+
+    return respuestas
+
 
 # @TODO leamos respuetas desde "frases.txt"
 def main(respuestas: list):
     print(random.choice(respuestas))
 
-
-main()
+if __name__ == "__main__":
+    main(respuestas_desde(FICHERO))
