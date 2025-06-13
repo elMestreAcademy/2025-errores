80fd1867bf2c2c332ac8ba6be831047905cd73a4
elMestre <carmesce@gmail.com>
2025-06-13 09:27:20 +0200

    Generador Aldea

--- generadores.py
+++ generadores.py
@@ -1,22 +1,51 @@
-from random import randint
+from random import randint, shuffle
 
 
 def funcion():
     i = 0
-    while(True):
+    while True:
         i += 1
         return i
 
 
 def generador():
     i = 0
-    while(True):
+    while True:
         i += 1
         yield i
+        print("GENERADO CONTINUA")
 
 
-gen = generador()
+def generadorDeAldeanos(nombres: list, apellidos: list) -> str:
+    shuffle(nombres)
+    shuffle(apellidos)
+    while nombres and apellidos:
+        yield f"{nombres.pop()} {apellidos.pop()}"
 
-for i in range(10):
-    print(f"FUNCION:   {funcion()}")
-    print(f"GENERADOR: {next(gen)}")
+    yield "NO QUEDA NADIE EN LA ALDEA"
+
+
+def demoBasica():
+    gen = generador()
+
+    for i in range(10):
+        print(f"FUNCION:   {funcion()}")
+        print(f"GENERADOR: {next(gen)}")
+
+
+def demoAldea():
+    contador = generador()
+    aldea = generadorDeAldeanos(
+        ["Paco", "Marta", "Lucia"],
+        ["Ortíz", "Gonzalez", "Pescadero", "Pérez", "Castillos"]
+    )
+
+    while True:
+        try:
+            print(f"{next(contador)} - {next(aldea)}")
+        except StopIteration:
+            break
+
+
+if __name__ == "__main__":
+    demoBasica()
